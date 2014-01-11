ad42ece20fdfe13c816caaeeee48673279977f43
DarkBoss <darkboss@PCMorgan.(none)>
2014-01-11 18:44:47 +0100

    Added mkdev: tool to create a developpement project, subdirs + Makefile\nUpdated installation script: now installs submodules individually

--- install.sh
+++ install.sh
@@ -1,12 +1,36 @@
-#!/bin/bah
+#!/bin/bash
 
 ####
-## Adding path to PATH env
+## Variables
 ####
 
 echo "===== sysTools installation ====="
 
-CMD=/opt/sysTools/cmd
-cat $CMD > /etc/paths.d/sysTools
+app_name="sysTools"
+default_path="/usr/bin"
+file_dir="./cmd"
+file_list="gitter"
 
-echo "===== Add PATH to PATH env ====="
+####
+## Asking install dir
+####
+read -e -i "$default_path" -p "Default installation path: " input
+path="${input:-$default_path}/" #$app_name
+
+####
+## Install modules
+####
+
+default_ans="Y"
+echo "===== Installation process ====="
+for f in $file_list; do
+	read -e -i "$default_ans" -p "Install $f (to: $path)? [Y|n] " ans
+	ans="${ans:-$default_ans}"
+	if [[ $ans == "Y" || $ans == "y" ]]; then
+		sudo install -D -t $path $file_dir/$f
+	fi
+#	inst_module="${ans:-Y"
+#	
+done
+
+exit 0
